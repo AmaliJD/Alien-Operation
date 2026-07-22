91472ab3e0d790f872ce17fefa47d0785c7b0743
AmaliJD <amali.jdelauney@gmail.com>
2026-07-22 17:52:28 -0400

    FIX BUG - mouse held on current ail complete

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -11,12 +11,15 @@ public class Main : MonoBehaviour
     int patientIndex = 0;
 
     const float AIL_RADIUS = 1;
+    bool readMouseDown;
 
     void Awake()
     {
         NewPatient().AddAilments(
-            new Ailment(5, 1, true, -3),
-            new Ailment(8, 2, false, 3)
+            new Ailment(5, 1, false, -3),
+            new Ailment(8, 2, false, 3),
+            new Ailment(5, 2, true, -3),
+            new Ailment(12, 3, false, 3)
         );
     }
 
@@ -41,22 +44,27 @@ public class Main : MonoBehaviour
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.value);
         bool mousePressed = Mouse.current.leftButton.value == 1;
 
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+            readMouseDown = true;
+
         Patient patient = patients[patientIndex];
         if (!patient.finished)
         {
             if (patient.IsCurrentFinished())
             {
+                readMouseDown = false;
                 patient.LoadNextAilments();
             }
 
             foreach (Ailment ail in patient.currentAilments)
             {
+                bool cursorOnAil = Vector2.Distance(mousePos, ail.location) <= AIL_RADIUS;
                 ail.DecrementTime();
-                GLGizmos.SetColor(ail.done ? Color.red : Color.white);
+                GLGizmos.SetColor(ail.done ? Color.red : (cursorOnAil ? Color.yellow : Color.white));
                 GLGizmos.DrawText(ail.displayTime.ToString(), ail.location, null, 10);
                 GLGizmos.DrawOpenCircle(ail.location, AIL_RADIUS);
 
-                if (!ail.done && mousePressed && Vector2.Distance(mousePos, ail.location) <= AIL_RADIUS)
+                if (!ail.done && mousePressed && cursorOnAil && readMouseDown)
                 {
                     ail.done = true;
                 }
